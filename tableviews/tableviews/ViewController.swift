30e014e539e2c722abb0d938d006bc830db76ff6
Daniel Colangelo <Danalfons@yahoo.com>
2017-03-09 01:23:25 -0500

    finished making the first table

--- tableviews/tableviews/ViewController.swift
+++ tableviews/tableviews/ViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    let cellContent = ["penny", "dewey", "presley", "kenzie"]
+    
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return cellContent.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = "Top row"
+        cell.textLabel?.text = cellContent[indexPath.row]
         
         return cell
     }
