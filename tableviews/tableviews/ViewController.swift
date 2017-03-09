ff97fb6eb2f4d6597d60d666aa44020fa17ad1bb
Daniel Colangelo <Danalfons@yahoo.com>
2017-03-09 00:39:19 -0500

    learning with tables
    playing around with table views.

---
+++ tableviews/tableviews/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  tableviews
+//
+//  Created by Danny Colangelo on 3/9/17.
+//  Copyright © 2017 Danny Colangelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Top row"
+        
+        return cell
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
